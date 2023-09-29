e9769b64519269ce329696bf70f5c9ab79f17ed5
DevNguyenPhuong <dev.nguyenphuong@gmail.com>
2023-09-29 09:28:31 +0700

    initial commit

---
+++ index_javascript.js
@@ -0,0 +1,48 @@
+// slider trang index
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img");
+const imgContainer = document.querySelector(".aspect-ratio-169");
+const dotItem = document.querySelectorAll(".dot");
+let imgNumber = imgPosition.length;
+let index = 0;
+imgPosition.forEach(function (image, index) {
+  image.style.left = index * 100 + "%";
+  dotItem[index].addEventListener("click", function () {
+    slider(index);
+  });
+});
+
+function imgSlide() {
+  index++;
+  //console.log(index)
+  if (index >= imgNumber) {
+    index = 0;
+  }
+  slider(index);
+}
+
+function slider(index) {
+  imgContainer.style.left = "-" + index * 100 + "%";
+  const dotActived = document.querySelector(".dot-actived");
+  dotActived.classList.remove("dot-actived");
+  dotItem[index].classList.add("dot-actived");
+}
+setInterval(imgSlide, 5000);
+
+// Popup
+
+let popup = document.getElementById("popup");
+let lines = Array.from(popup.getElementsByClassName("line"));
+
+lines.forEach((line, index) => {
+  line.style.setProperty("--line-index", index);
+});
+
+let buttonShow = document.getElementById("show-popup");
+buttonShow.addEventListener("click", () => {
+  popup.style.display = "inline-block";
+});
+
+let buttonClose = document.getElementById("close-popup");
+buttonClose.addEventListener("click", () => {
+  popup.style.display = "none";
+});
